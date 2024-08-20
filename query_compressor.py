208722591de85e00225237221e6c4cfab8b75a0f
Piero Savastano <piero.savastano@gmail.com>
2024-08-21 00:53:19 +0200

    first version

---
+++ query_compressor.py
@@ -0,0 +1,22 @@
+
+
+from cat.log import log
+from cat.mad_hatter.decorators import hook
+
+@hook
+def cat_recall_query(user_message, cat):
+    
+
+    conversation_so_far  = cat.stringify_chat_history(latest_n=5)
+
+    prompt = f"""Here is a conversation between an AI and a Human:
+{conversation_so_far}
+
+Sum up in a single sentence what the human wants, keeping all the details from the conversation.
+The sentence must be expressed from the point of view of the human.
+
+Sentence: """
+
+    compressed_query = cat.llm(prompt)
+
+    return compressed_query
